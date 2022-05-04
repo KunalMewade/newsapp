903716f53c228a00439a5a883ccd0b69bae9a96f
kunal <kunalmewade64@gmail.com>
2022-05-04 12:00:56 +0530

    added top loading bar successfully

--- src/components/News.js
+++ src/components/News.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropsType from 'prop-types';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export default class News extends Component {
 
@@ -18,8 +19,8 @@ export default class News extends Component {
 
   }
 
-  capitalizeFirstLetter=(string)=>{
-     return string.charAt(0).toUpperCase()+string.slice(1);
+  capitalizeFirstLetter = (string) => {
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
   constructor(props) {
@@ -27,28 +28,32 @@ export default class News extends Component {
     console.log("hellow i am constructor from News.js");
     this.state = {
       articles: [],
-      loading: false,
-      page: 1
+      loading: true,
+      page: 1,
+      totalResults: 0
     }
     document.title = `${this.capitalizeFirstLetter(this.props.category)} - NewsAPI`;
   }
 
   async updateNews() {
-
+    this.props.setProgress(10);
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2de821f8e3e0423fbd8f140c881dd70d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ laoding: true })
     let data = await fetch(url);
+    this.props.setProgress(30);
     let parsedData = await data.json();
-
+    this.props.setProgress(70);
+    
     console.log(parsedData)
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
       loading: false
     })
-
+    this.props.setProgress(100);
+    
   }
-
+  
   async componentDidMount() {
     console.log("CDM");
     // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2de821f8e3e0423fbd8f140c881dd70d&page=1&pageSize=${this.props.pageSize}`;
@@ -81,7 +86,7 @@ export default class News extends Component {
     //   loading: true
     // })
     console.log("previous");
-    this.setState({page:this.state.page - 1});
+    this.setState({ page: this.state.page - 1 });
     this.updateNews();
   }
 
@@ -100,29 +105,49 @@ export default class News extends Component {
     //   })
     // }
     console.log("next");
-    this.setState({page:this.state.page + 1});
+    this.setState({ page: this.state.page + 1 });
     this.updateNews();
 
   }
+
+  fetchMoreData = async () => {
+    this.setState({ page: this.state.page + 1 });
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2de821f8e3e0423fbd8f140c881dd70d&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parsedData = await data.json();
+
+    console.log(parsedData)
+    this.setState({
+      articles: this.state.articles.concat(parsedData.articles),
+      totalResults: parsedData.totalResults
+    })
+
+  };
   render() {
     console.log("render")
     return (
-      <div className="container my-3">
+      <>
         <h1 className='text-center'>NewsAPI - Top {this.capitalizeFirstLetter(this.props.category)} Headlines</h1>
         <hr />
         {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading && this.state.articles.map((element) => {
-            return <div className="col-md-4" key={element.url}>
-              <NewsItem title={element.title?.slice(1, 45)} description={element.description?.slice(1, 88)} imageurl={element.urlToImage} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
-            </div>
-          })}
-        </div>
-        <div className="container d-flex justify-content-between">
-          <button type="button" disabled={this.state.page <= 1} className="btn btn-dark" onClick={this.handlePreviousClick}>&larr; Previous</button>
-          <button type="button" disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-        </div>
-      </div>
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner />}
+        >
+        <div className="container">
+          <div className="row">
+            {this.state.articles.map((element) => {
+              return <div className="col-md-4" key={element.url}>
+                <NewsItem title={element.title?.slice(1, 45)} description={element.description?.slice(1, 88)} imageurl={element.urlToImage} newsURL={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
+              </div>
+            })}
+          </div>
+          </div>
+        </InfiniteScroll>
+
+      </>
     )
   }
 }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
- 
+
   render() {
-    let { title, description, imageurl, newsURL,author,date,source} = this.props;
+    let { title, description, imageurl, newsURL, author, date, source } = this.props;
     return (
       <div className='my-3'>
         <div className="card" >
-        <span className='position-absolute top-0  translate-middle badge rounded-pill bg-danger' style={{left:'90%', zIndex:'1'}}>{source}</span>
-          <img src={imageurl==null ? 'https://cdn.kalingatv.com/wp-content/uploads/2022/03/check-fake-news-on-WhatsApp.jpg' :imageurl } className="card-img-top" alt="img" />
+          <div style={{
+                        display: 'flex',
+                        justifyContent: 'flex-end',
+                        position:'absolute',
+                        right:'0'
+                   }}>
+            <span className=' badge rounded-pill bg-danger' >{source}</span>
+
+          </div>
+          <img src={imageurl == null ? 'https://cdn.kalingatv.com/wp-content/uploads/2022/03/check-fake-news-on-WhatsApp.jpg' : imageurl} className="card-img-top" alt="img" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <p className='card-text'><small className='text-muted'>By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+            <p className='card-text'><small className='text-muted'>By {!author ? "Unknown" : author} on {new Date(date).toGMTString()}</small></p>
             <a href={newsURL} target='_blank' rel='noreferrer noopener' className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
